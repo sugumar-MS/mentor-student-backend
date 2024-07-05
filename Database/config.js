79b85e0fb4537c4fbfc3c2b76d2d813999fb6cec
sugumar-MS <msugumar832@gamil.com>
2024-07-05 23:58:24 +0530

    addedREADME.md

--- Database/config.js
+++ Database/config.js
@@ -14,7 +14,6 @@ const connectDB = async () => {
             
         });
         console.log("MongoDB Connected...");
-       
     } catch (error) {
         console.error("MongoDB connection error:", error.message);
     }
